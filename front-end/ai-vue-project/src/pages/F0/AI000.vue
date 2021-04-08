fbd636632f0484f8d5b1e74235daab11e3cc7970
Michael Lin <yanwun1214@gmail.com>
2021-04-08 23:20:19 +0800

    #comment : update eslint&prettier setting

--- front-end/ai-vue-project/src/pages/F0/AI000.vue
+++ front-end/ai-vue-project/src/pages/F0/AI000.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="title">
-      AI index
-  </div>
+  <div class="title">AI index</div>
 </template>
 
 <script>
 export default {
   name: 'AI000',
 };
-
 </script>
 
 <style scoped>
-    .title{
-        color:green;
-    }
+.title {
+  color: green;
+}
 </style>
