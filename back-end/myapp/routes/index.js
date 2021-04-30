b98f320c8f11b884ec492cc677e520382ae0dff6
@x0mg <jeff14994@gmail.com>
2021-04-30 18:21:55 +0800

    Complete Get/POST unit test
    - Get specific user by id
    - POST data to DB
    - Add description of unit test

--- back-end/myapp/routes/index.js
+++ back-end/myapp/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '0M9, WH0m 4M1 , wH3r3 4m1 1, WhY M 1 h3R3 ???' });
 });
 
 module.exports = router;
