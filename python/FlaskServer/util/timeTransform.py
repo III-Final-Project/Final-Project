c6d5476e1f1492a1b89a4573a61fe00b3e67cbb0
Lin Michael <yanwun1214@gmail.com>
2021-06-10 22:54:24 +0800

    Comment: update AIA000 and build FashionService Detection.

--- python/FlaskServer/util/timeTransform.py
+++ python/FlaskServer/util/timeTransform.py
@@ -9,5 +9,15 @@ def transform_local_time(now):
     hour = time_structure.tm_hour
     minute = time_structure.tm_min
     second = time_structure.tm_sec
+    if minute < 10:
+        minute = '0{}'.format(str(minute))
+    if hour < 10:
+        hour = '0{}'.format(str(hour))
+    if month < 10:
+        month = '0{}'.format(str(month))
+    if day < 10:
+        month = '0{}'.format(str(day))
+    if second < 10:
+        second = '0{}'.format(str(second))
     result = '{}-{}-{} {}:{}:{}'.format(year, month, day, hour, minute, second)
     return result
