27f530f7b32089ec7a94920a4df7e5aec11986a8
@x0mg <jeff14994@gmail.com>
2021-06-12 23:53:52 +0800

    Complete obtain top three colors from an image
    - Check the most appear color by numpy filter and count the amout
    - Sort and find out the top three colors

--- python/FlaskServer/color_calculate/colorCalculate.py
+++ python/FlaskServer/color_calculate/colorCalculate.py
@@ -1,6 +1,7 @@
 # import the necessary packages
 import numpy as np
 import cv2
+import json
 # Load the image
 img_path = './resources/'
 # image = cv2.imread(img_path + 'palette.png')
@@ -44,37 +45,58 @@ boundaries = [
     # ([136, 43, 46], [140, 255, 255]),
     ([141, 43, 46], [145, 255, 255]),
 ]
-index = 0
-rank = {}
-# Loop over the boundaries
-for (lower, upper) in boundaries:
-    index += 1
-    # Create NumPy arrays from the boundaries
-    lower = np.array(lower, dtype = "uint8")
-    upper = np.array(upper, dtype = "uint8")
-    # Find the colors within the specified boundaries and apply the mask
-    mask = cv2.inRange(image, lower, upper)
-    #  Mask - Filter the specific color
-    output = cv2.bitwise_and(image, image, mask = mask)
-    # Check the most appear color
-    count = (output > 1).sum()
-    # Store the result in dict 
-    rank[index] = count
-    # Sort the result from large to small
-    print(sorted(rank.items(), key=lambda item: item[1], reverse=True))
-    # Change HSV to BGR(cv2 default)   
-    output = cv2.cvtColor(output, cv2.COLOR_HSV2BGR)
+def obtain_top_colors(image):
+    """
+    Return top three colors appeared in an image
     
+    @image: image from OpenCV imread
+        
+    Return format:
+        # {
+        # "Top color": "blue",
+        # "Secondary color": "brown",
+        # "Third color": "black"
+    # }
+    """
+    index = 0
+    rank = {}
+    # Loop over the boundaries
+    for (lower, upper) in boundaries:
+        index += 1
+        # Create NumPy arrays from the boundaries
+        lower = np.array(lower, dtype = "uint8")
+        upper = np.array(upper, dtype = "uint8")
+        # Find the colors within the specified boundaries and apply the mask
+        mask = cv2.inRange(image, lower, upper)
+        #  Mask - Filter the specific color
+        output = cv2.bitwise_and(image, image, mask = mask)
+        # Check the most appear color
+        count = (output > 1).sum()
+        # Store the result in dict 
+        rank[index] = count
+        # Sort the result from large to small
+        top_three_color = sorted(rank.items(), key=lambda item: item[1], reverse=True)[:3]
+        # Change HSV to BGR(cv2 default)   
+        output = cv2.cvtColor(output, cv2.COLOR_HSV2BGR)
+        # Change image positions
+        # cv2.namedWindow('images')
+        # cv2.moveWindow('images', 100, 100)
+        # Show the images
+        cv2.imshow("images", np.hstack([image_original, output]))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        cv2.waitKey(1)
     # Return three top colors
-
-    # output1 = np.hstack([image, output])
-    # plt.imshow(output1)
-    # plt.show()
-    # Change image positions
-    # cv2.namedWindow('images')
-    # cv2.moveWindow('images', 100, 100)
-    # Show the images
-    # cv2.imshow("images", np.hstack([image_original, output]))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # cv2.waitKey(1)
+    json_header = ["Top color", "Secondary color",  "Third color"]
+    return_format = {}
+    # Data processing as dict
+    for i, ele in enumerate(json_header):
+        return_format[ele] = top_three_color[i][0]
+    # print(return_format)
+    # TODO: Convert number to exact color
+    # Dict to json
+    json_string = json.dumps(return_format)
+    print(json_string)
+    return json_string
+if __name__ == '__main__':
+    obtain_top_colors(image)
